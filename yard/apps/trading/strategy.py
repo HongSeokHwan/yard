a6ddc63f2e4937c777a0017f7323ed5e25a1e5cc
SeokHwan Hong <sh.hong@decemberco.com>
2014-08-04 21:32:04 +0900

    make strategy skeleton

--- yard/apps/trading/strategy.py
+++ yard/apps/trading/strategy.py
@@ -1,10 +1,76 @@
+import time
+
 from yard.utils.log import LoggableMixin
 from yard.utils.meta import SingletonMixin
+
+
+class Account(LoggableMixin):
+    pass
+
+
+class AccountManager(LoggableMixin):
+    pass
+
+
+class Quote(LoggableMixin):
+    def __init__(self):
+        super(Quote, self).__init__()
+
+
+class QuoteBoard(LoggableMixin, SingletonMixin):
+    def __init__(self):
+        super(QuoteBoard, self).__init__()
+        self.quotes = {}
+
+    def append(self, code, quote):
+        pass
+
+
+class Instrument(LoggableMixin):
+    def __init__(self):
+        pass
+
+
+class InstrumentManager(LoggableMixin):
+    pass
+
+
+class IStrategy(LoggableMixin):
+    def __init__(self):
+        super(IStrategy, self).__init__()
+
+    def run(self):
+        pass
+
+
+class BtcArb(IStrategy):
+    def __init__(self):
+        super(BtcArb, self).__init__()
+
+    def run(self):
+        pass
+
+
+class QuoteLogger(IStrategy):
+    def __init__(self):
+        pass
+
+
+class StrategyManager(LoggableMixin, SingletonMixin):
+    def __init__(self):
+        super(StrategyManager, self).__init__()
+        self.strategies = {}
+
+    def register_strategy(self, name, strategy):
+        self.strategies[name] = strategy
+
+    def run(self):
+        while True:
+            time.sleep(0.1)
 
 
 class StrategyRunner(LoggableMixin, SingletonMixin):
     def start(self):
         self.info('Start running')
-
-        while True:
-            pass
+        StrategyManager().register_strategy('btc_arb', BtcArb())
+        StrategyManager().run()
--- yard/sandbox/quotecollector.py
+++ yard/sandbox/quotecollector.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import time
+import json
 import datetime
 import urllib2
 import MySQLdb as mdb
@@ -19,6 +20,90 @@ ICBIT_CODE_1412 = 'BUZ4'
 KORBIT_CODE = 'KORBIT'
 BITSTAMP_CODE = 'BITSTAMP'
 USDKRW = 'USDKRW'
+
+
+class Quote:
+    def __init__(self):
+        self.code = ''
+        self.bid_prices = []
+        self.ask_prices = []
+
+        self.bid_quantities = []
+        self.ask_quantities = []
+
+    def bitstamp_to_quote(self, bitstamp_quote):
+        self.code = BITSTAMP_CODE
+        try:
+            j = json.loads(bitstamp_quote)
+            i = 0
+            while True:
+                if i >= 10:
+                    break
+
+                self.bid_prices[i] = float(j['bids'][i][0])
+                self.bid_quantities[i] = float(j['bids'][i][1])
+                self.ask_prices[i] = float(j['asks'][i][0])
+                self.ask_quantities[i] = float(j['asks'][i][1])
+
+                i += 1
+            return True
+
+        except Exception, e:
+            print str(e)
+            return False
+
+    def korbit_to_quote(self, korbit_quote):
+        self.code = KORBIT_CODE
+        try:
+            j = json.loads(korbit_quote)
+            i = 0
+            while True:
+                if i >= 10:
+                    break
+
+                self.bid_prices[i] = float(j['bids'][i][0])
+                self.bid_quantities[i] = float(j['bids'][i][1])
+                self.ask_prices[i] = float(j['asks'][i][0])
+                self.ask_quantities[i] = float(j['asks'][i][1])
+
+                i += 1
+            return True
+
+        except Exception, e:
+            print str(e)
+            return False
+
+    def icbit_to_quote(self, code, icbit_quote):
+        self.code = code
+        try:
+            j = json.loads(icbit_quote)
+
+            i = 0
+            while True:
+                if i >= 10:
+                    break
+
+                self.bid_prices[i] = float(j['buy'][i]['p'])
+                self.bid_quantities[i] = float(j['buy'][i]['q'])
+
+                self.ask_prices[i] = float(j['ask'][i]['p'])
+                self.ask_quantities[i] = float(j['ask'][i]['q'])
+
+                i += 1
+            return True
+
+        except Exception, e:
+            print str(e)
+            return False
+
+    def to_database(self, con, dt):
+        # TODO change query statement
+        with con:
+            cur = con.cursor()
+            query = "INSERT INTO quote(dt, code, value) VALUES ("
+            "'%s', '%s', '%s')" % (datetime, self.code, '')
+            cur.execute(query)
+        pass
 
 
 def get_quote(url):
