7e59a21a812b603b16a4f3f84449c40da7183569
SeokHwan Hong <sh.hong@decemberco.com>
2014-08-04 23:02:51 +0900

    implement collect detail quote

--- yard/sandbox/collectdetailquote.py
+++ yard/sandbox/collectdetailquote.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import time
+import json
 import datetime
 import urllib2
 import MySQLdb as mdb
@@ -12,7 +13,7 @@ ICBIT_URL_TEMPLATE = 'https://api.icbit.se/api/orders/book?ticker=%s'
 KORBIT_URL = 'https://api.korbit.co.kr/v1/orderbook'
 BITSTAMP_URL = 'https://www.bitstamp.net/api/order_book/'
 CURRENCY_URL_TEMPLATE = 'http://www.webservicex.net/CurrencyConvertor.asmx/' \
-                            'ConversionRate?FromCurrency=%s&ToCurrency=%s'
+    'ConversionRate?FromCurrency=%s&ToCurrency=%s'
 
 ICBIT_CODE_1409 = 'BUU4'
 ICBIT_CODE_1412 = 'BUZ4'
@@ -20,61 +21,226 @@ KORBIT_CODE = 'KORBIT'
 BITSTAMP_CODE = 'BITSTAMP'
 USDKRW = 'USDKRW'
 
+
+class Quote:
+    query_template = """
+    insert into detail_quotes (
+    dt, code, 
+
+    bid_price1, bid_price2, bid_price3, bid_price4, bid_price5, 
+    bid_price6, bid_price7, bid_price8, bid_price9, bid_price10, 
+
+    ask_price1, ask_price2, ask_price3, ask_price4, ask_price5, 
+    ask_price6, ask_price7, ask_price8, ask_price9, ask_price10, 
+
+    bid_quantity1, bid_quantity2, bid_quantity3, bid_quantity4, bid_quantity5, 
+    bid_quantity6, bid_quantity7, bid_quantity8, bid_quantity9, bid_quantity10, 
+
+    ask_quantity1, ask_quantity2, ask_quantity3, ask_quantity4, ask_quantity5, 
+    ask_quantity6, ask_quantity7, ask_quantity8, ask_quantity9, ask_quantity10)
+
+    VALUES ('%s', '%s', %s %s %s %s)"""
+
+    ten_array_template_with_comma = """
+    %f, %f, %f, %f, %f,
+    %f, %f, %f, %f, %f,"""
+
+    ten_array_template_without_comma = """
+    %f, %f, %f, %f, %f,
+    %f, %f, %f, %f, %f"""
+
+    def __init__(self):
+        self.code = ''
+        self.bid_prices = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.ask_prices = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        self.bid_quantities = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.ask_quantities = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    def bitstamp_to_quote(self, bitstamp_quote):
+        self.code = BITSTAMP_CODE
+        try:
+            j = json.loads(bitstamp_quote)
+            i = 0
+            while True:
+                if i >= 10:
+                    break
+
+                self.bid_prices[i] = float(j['bids'][i][0])
+                self.bid_quantities[i] = float(j['bids'][i][1])
+                self.ask_prices[i] = float(j['asks'][i][0])
+                self.ask_quantities[i] = float(j['asks'][i][1])
+
+                i += 1
+            return True
+
+        except Exception, e:
+            print str(e)
+            return False
+
+    def usdkrw_to_quote(self, quote):
+        self.code = USDKRW
+        self.bid_prices[0] = quote
+        self.ask_prices[0] = quote
+
+    def korbit_to_quote(self, korbit_quote):
+        self.code = KORBIT_CODE
+        try:
+            j = json.loads(korbit_quote)
+            i = 0
+            while True:
+                if i >= 10:
+                    break
+
+                self.bid_prices[i] = float(j['bids'][i][0])
+                self.bid_quantities[i] = float(j['bids'][i][1])
+                self.ask_prices[i] = float(j['asks'][i][0])
+                self.ask_quantities[i] = float(j['asks'][i][1])
+
+                i += 1
+            return True
+
+        except Exception, e:
+            print str(e)
+            return False
+
+    def icbit_to_quote(self, code, icbit_quote):
+        self.code = code
+        try:
+            j = json.loads(icbit_quote)
+
+            i = 0
+            while True:
+                if i >= 10:
+                    break
+
+                self.bid_prices[i] = float(j['buy'][i]['p'])
+                self.bid_quantities[i] = float(j['buy'][i]['q'])
+                self.ask_prices[i] = float(j['sell'][i]['p'])
+                self.ask_quantities[i] = float(j['sell'][i]['q'])
+
+                i += 1
+            return True
+
+        except Exception, e:
+            print str(e)
+            return False
+
+    def create_query(self, dt):
+        bid_price_str = Quote.ten_array_template_with_comma % (
+                self.bid_prices[0], self.bid_prices[1], 
+                self.bid_prices[2], self.bid_prices[3], 
+                self.bid_prices[4], self.bid_prices[5],
+                self.bid_prices[6], self.bid_prices[7], 
+                self.bid_prices[8], self.bid_prices[9])
+
+        ask_price_str = Quote.ten_array_template_with_comma % (
+                self.ask_prices[0], self.ask_prices[1], 
+                self.ask_prices[2], self.ask_prices[3], 
+                self.ask_prices[4], self.ask_prices[5],
+                self.ask_prices[6], self.ask_prices[7], 
+                self.ask_prices[8], self.ask_prices[9])
+
+        bid_quantity_str = Quote.ten_array_template_with_comma % (
+                self.bid_quantities[0], self.bid_quantities[1], 
+                self.bid_quantities[2], self.bid_quantities[3], 
+                self.bid_quantities[4], self.bid_quantities[5],
+                self.bid_quantities[6], self.bid_quantities[7], 
+                self.bid_quantities[8], self.bid_quantities[9])
+
+        ask_quantity_str = Quote.ten_array_template_without_comma % (
+                self.ask_quantities[0], self.ask_quantities[1], 
+                self.ask_quantities[2], self.ask_quantities[3], 
+                self.ask_quantities[4], self.ask_quantities[5],
+                self.ask_quantities[6], self.ask_quantities[7], 
+                self.ask_quantities[8], self.ask_quantities[9])
+
+        query = Quote.query_template % (
+                dt, self.code,
+                bid_price_str,
+                ask_price_str,
+                bid_quantity_str,
+                ask_quantity_str)
+
+        #print 'query', query
+        return query
+
+    def to_database(self, con, dt):
+        with con:
+            cur = con.cursor()
+            query = self.create_query(dt)
+            cur.execute(query)
+
+
 def get_quote(url):
     req = urllib2.Request(url)
-    req.add_unredirected_header('User-Agent',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, ' \
-            'like Gecko) Chrome/26.0.1410.64 Safari/537.31')
+    req.add_unredirected_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; '
+                                'WOW64) AppleWebKit/537.31 (KHTML, '
+                                'like Gecko) Chrome/26.0.1410.64 Safari/537.31')
     source = urllib2.urlopen(req).read()
     return source
+
 
 def get_currency(from_currency, to_currency):
     url = CURRENCY_URL_TEMPLATE % (from_currency, to_currency)
     return get_quote(url)
+
 
 def parse_currency_value(content):
     root = ET.fromstring(content)
     return root.text
 
 
-def insert_into_db(con, datetime, code, value):
-    with con:
-        cur = con.cursor()
-        query = "INSERT INTO quote(dt, code, value) VALUES ('%s', '%s', '%s')"\
-                % (datetime, code, value)
-        cur.execute(query)
-
 def _main(con):
-    try:
-        print 'start get quote'
+    print 'start collectdetailquote.'
 
-        icbit_quote_1409 = get_quote(ICBIT_URL_TEMPLATE % ICBIT_CODE_1409)
-        icbit_quote_1412 = get_quote(ICBIT_URL_TEMPLATE % ICBIT_CODE_1412)
-        korbit_quote = get_quote(KORBIT_URL)
-        bitstamp_quote = get_quote(BITSTAMP_URL)
-        usdkrw_content = get_currency('USD', 'KRW')
-        usdkrw_value = parse_currency_value(usdkrw_content)
+    raw_icbit_quote_1409 = get_quote(ICBIT_URL_TEMPLATE % ICBIT_CODE_1409)
+    raw_icbit_quote_1412 = get_quote(ICBIT_URL_TEMPLATE % ICBIT_CODE_1412)
+    raw_korbit_quote = get_quote(KORBIT_URL)
+    raw_bitstamp_quote = get_quote(BITSTAMP_URL)
 
-        now = datetime.datetime.now()
-        dt = time.strftime('%Y-%m-%d %H:%M:%S')
+    usdkrw_content = get_currency('USD', 'KRW')
+    usdkrw_value = float(parse_currency_value(usdkrw_content))
+    usdkrw_quote = Quote()
+    usdkrw_quote.usdkrw_to_quote(usdkrw_value)
 
-        insert_into_db(con, dt, ICBIT_CODE_1409, icbit_quote_1409)
-        insert_into_db(con, dt, ICBIT_CODE_1412, icbit_quote_1412)
-        insert_into_db(con, dt, KORBIT_CODE, korbit_quote)
-        insert_into_db(con, dt, BITSTAMP_CODE, bitstamp_quote)
-        insert_into_db(con, dt, USDKRW, usdkrw_value)
+    icbit_quote_1409 = Quote()
+    icbit_quote_1409.icbit_to_quote(ICBIT_CODE_1409, raw_icbit_quote_1409)
 
-        print 'end get quote and insert into db.', now
-        time.sleep(30)
-    except:
-        pass
+    icbit_quote_1412 = Quote()
+    icbit_quote_1412.icbit_to_quote(ICBIT_CODE_1412, raw_icbit_quote_1412)
+
+    korbit_quote = Quote()
+    korbit_quote.korbit_to_quote(raw_korbit_quote)
+
+    bitstamp_quote = Quote()
+    bitstamp_quote.bitstamp_to_quote(raw_bitstamp_quote)
+
+    now = datetime.datetime.now()
+    dt = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    icbit_quote_1409.to_database(con, dt)
+    icbit_quote_1412.to_database(con, dt)
+    korbit_quote.to_database(con, dt)
+    bitstamp_quote.to_database(con, dt)
+    usdkrw_quote.to_database(con, dt)
+
+    print 'end get quote and insert into db.', now
+    time.sleep(30)
+
 
 def main():
-
-    con = mdb.connect('localhost', 'root', 'baadf00d', 'yard');
+    con = mdb.connect('localhost', 'root', 'baadf00d', 'yard')
     while True:
-        _main(con)
+        try:
+            _main(con)
+        except:
+            try:
+                con.close()
+            except:
+                pass
+            con = mdb.connect('localhost', 'root', 'baadf00d', 'yard')
+
 
 if __name__ == '__main__':
     main()
-
