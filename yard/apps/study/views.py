4c81cf29b9c51d8581a39896ddc5cf326ecb50fd
SeokHwan Hong <sh.hong@decemberco.com>
2014-08-20 03:48:09 +0900

    Add system info page

--- yard/apps/study/views.py
+++ yard/apps/study/views.py
@@ -1,11 +1,17 @@
+import subprocess
+from subprocess import PIPE
+
+import MySQLdb as mdb
+
 #from django.shortcuts import render
 from django.http import HttpResponse
 
 #from yard.apps.trading import strategy
+from yard import settings
 from yard.utils.tocsv import to_csv
 
 
-def index(request):
+def help(request):
     content = """
     Usage:
 http://www.jiref.com/study/quotedata?codes=BITSTAMP&codes=KORBIT&codes=BTCCHINA&codes=USDKRW&codes=CNYKRW&start_date=2014-01-01&end_date=2014-12-31&column=*
@@ -28,6 +34,66 @@ column = [
 ]
     """
     return HttpResponse('<br />'.join(content.split('\n')))
+
+
+def _get_ps_result():
+    p1 = subprocess.Popen(['ps', 'aux'], stdout=PIPE)
+    p2 = subprocess.Popen(['grep', 'collectdetailquote'],
+                          stdin=p1.stdout, stdout=PIPE)
+    p1.stdout.close()
+    process_list = p2.communicate()[0]
+    return process_list
+
+
+def _get_df_result():
+    p = subprocess.Popen(['df', '-h'], stdout=PIPE)
+    result = p.communicate()[0]
+    print result
+    return result
+
+
+def _get_db_last_updated_date():
+    db_info = settings.PERIODIC_QUOTE_DATABASE
+    con = mdb.connect(host=db_info['host'],
+                      port=db_info['port'],
+                      user=db_info['user'],
+                      passwd=db_info['passwd'],
+                      db=db_info['database'])
+
+    query = 'select dt from detail_quotes order by dt desc limit 1'
+    with con:
+        cur = con.cursor(mdb.cursors.DictCursor)
+        cur.execute(query)
+        rows = cur.fetchall()
+        return rows
+    return ''
+
+
+def systeminfo(request):
+    template = """
+    1. ps -aux | grep 'collectdetailquote.py'
+    %s
+
+    2. df -h
+    %s
+
+    3. database last updated
+    %s
+
+    """
+
+    ps = _get_ps_result()
+    df = _get_df_result()
+    last_updated = _get_db_last_updated_date()
+
+    html = '<br />'.join((template % (ps, df, last_updated)).split('\n'))
+
+    return HttpResponse(html)
+
+
+def index(request):
+    return systeminfo(request)
+    #return help(request)
 
 
 def quotedata(request):
--- yard/urls.py
+++ yard/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from yard.apps.study import views
+
 urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^study/', include('yard.apps.study.urls')),)
