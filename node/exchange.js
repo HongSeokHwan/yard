5501b83bf2a41067f0d3ec2d39e080724d0ea550
Minjong Chung <mjipeo@gmail.com>
2014-08-21 16:30:40 +0900

    Implement the Btcchina exchange

--- node/exchange.js
+++ node/exchange.js
@@ -3,46 +3,68 @@ var util = require('util');
 var _ = require('underscore');
 var equal = require('deep-equal');
 var request = require('request');
+var io = require('socket.io-client');
 var ws = require('ws');
 var config = require('./config');
 var logger = require('./logger');
 
 
-// Serializer
-// ----------
+// Poller
+// ------
 
-var JSONSerializer = function () {};
+var Poller = function (options) {
+  events.EventEmitter.call(this);
 
-JSONSerializer.prototype.loadMessage = function (raw) {
-  return JSON.parse(raw);
+  options = options || {};
+  this.url = options.url;
+  this.serializer = options.serializer || JSON.parse;
+  this.normalizer = options.normalizer || function (tick) { return tick; };
+  this.concurrency = options.concurrency || 1;
+  this.comparator = options.comparator
+                    || function (new_, old) { return !equal(new_, old); }
+  this.label = options.label || 'Poller';
+  this.last = null;
 };
 
-JSONSerializer.prototype.dumpMessage = function (message) {
-  return JSON.stringify(message);
+util.inherits(Poller, events.EventEmitter);
+
+Poller.prototype.start = function () {
+  !!this.url && _.range(this.concurrency).forEach(this._poll.bind(this));
 };
 
+Poller.prototype._poll = function () {
+  var self = this;
+  var poll = function () {
+    logger.debug('Polling %s', self.label);
 
-var XMLSerializer = function () {};
+    request(self.url, function (error, response, body) {
+      // TODO: Error handling
+      raw = self.serializer(body);
+      tick = self.normalizer(raw);
+      if (!tick) {
+        return;
+      }
 
-XMLSerializer.prototype.loadMessage = function (raw) {
-  // FIXME: Implement
-  return raw;
-};
+      if (!self.last || self.comparator(tick, self.last)) {
+        first = !self.last;
+        self.last = tick;
+        !first && self.emit('tick', tick);
+      }
 
-XMLSerializer.prototype.dumpMessage = function (message) {
-  // FIXME: Implement
-  return message;
+      poll();
+    });
+  };
+
+  poll();
 };
 
 
 // Base
 // ----
 
-var Exchange = exports.Exchange = function (options) {
+var Exchange = exports.Exchange = function () {
   events.EventEmitter.call(this);
 
-  options = options || {};
-  this.serializer = options.serializer || new JSONSerializer();
   this.exchangeCode = this.constructor.exchangeCode;
 };
 
@@ -54,150 +76,45 @@ Exchange.prototype.normalizeTrade = function (tick) { return tick; };
 
 Exchange.exchangeCode = null;
 
-Exchange.currency = 'usd';
-
 util.inherits(Exchange, events.EventEmitter);
-
-
-// Websocket
-// ---------
-
-var WebsocketExchange = function (options) {
-  Exchange.call(this, options);
-
-  this.websocketUrl = options.websocketUrl;
-  this.connection = null;
-};
-
-util.inherits(WebsocketExchange, Exchange);
-
-WebsocketExchange.prototype.start = function () {
-  var self = this;
-  this.connection = new ws(this.websocketUrl);
-  this.connection.on('open', this.onConnect.bind(this));
-  this.connection.on('message', function (raw) {
-    self.onMessage(self.serializer.loadMessage(raw));
-  });
-};
-
-WebsocketExchange.prototype.onConnect = function () {};
-
-WebsocketExchange.prototype.onMessage = function (message) {};
-
-
-// Polling
-// -------
-
-var PollingExchange = function (options) {
-  Exchange.call(this, options);
-
-  this.quotePollUrl = options.quotePollUrl;
-  this.tradePollUrl = options.tradePollUrl;
-  this.quotePollConcurrency = options.quotePollConcurrency || 1;
-  this.tradePollConcurrency = options.tradePollConcurrency || 1;
-  this.lastQuote = null;
-  this.lastTrade = null;
-};
-
-util.inherits(PollingExchange, Exchange);
-
-PollingExchange.prototype.start = function () {
-  var self = this;
-
-  !!self.quotePollUrl && _.range(self.quotePollConcurrency).forEach(function () {
-    self._pollQuote();
-  });
-  !!self.tradePollUrl && _.range(self.tradePollConcurrency).forEach(function () {
-    self._pollTrade();
-  });
-};
-PollingExchange.prototype._pollQuote = function () {
-  var self = this;
-  var poll = function () {
-    logger.debug('%s - Polling quote', self.exchangeCode);
-
-    request(self.quotePollUrl, function (error, response, body) {
-      // TODO: Error handling
-      tick = self.serializer.loadMessage(body);
-      quote = self.normalizeQuote(tick);
-      if (!quote) {
-        return;
-      }
-
-      if (!equal(quote, self.lastQuote)) {
-        first = !!self.lastQuote;
-        self.lastQuote = quote;
-        !first && self.emit('tick', 'quote', quote);
-      }
-
-      poll();
-    });
-  };
-
-  poll();
-};
-PollingExchange.prototype._pollTrade = function () {
-  var self = this;
-  var poll = function () {
-    logger.debug('%s - Polling trade', self.exchangeCode);
-
-    request(self.tradePollUrl, function (error, response, body) {
-      // TODO: Error handling
-      tick = self.serializer.loadMessage(body);
-      trade = self.normalizeTrade(tick);
-      if (!trade) {
-        return;
-      }
-
-      if (!equal(trade, self.lastTrade)) {
-        first = !!self.lastTrade;
-        self.lastTrade = trade;
-        !first && self.emit('tick', 'trade', trade);
-      }
-
-      poll();
-    });
-  };
-
-  poll();
-};
 
 
 // Bitstamp
 // --------
 
 var BitstampExchange = exports.BitstampExchange = function (options) {
-  WebsocketExchange.call(this, _.defaults({
-    websocketUrl: 'wss://ws.pusherapp.com/app/de504dc5763aeef9ff52?protocol=7'
-                  + '&client=js&version=2.1.6&flash=false'
-  }, options));
+  Exchange.call(this);
 
+  this.url = 'wss://ws.pusherapp.com/app/de504dc5763aeef9ff52?protocol=7\
+              &client=js&version=2.1.6&flash=false'
   this.channels = ['order_book', 'live_trades'];
   this.ticker = config.tickers['bitstamp'];
 };
 
-util.inherits(BitstampExchange, WebsocketExchange);
+util.inherits(BitstampExchange, Exchange);
 
-BitstampExchange.prototype.onConnect = function () {
+BitstampExchange.prototype.start = function () {
   var self = this;
-  this.channels.forEach(function (channel) {
-    self.connection.send(self.serializer.dumpMessage({
-      event: 'pusher:subscribe',
-      data: { channel: channel }
-    }));
+  var socket = new ws(this.url);
+  socket.on('open', function () {
+    self.channels.forEach(function (channel) {
+      socket.send(JSON.stringify({
+        event: 'pusher:subscribe',
+        data: { channel: channel }
+      }));
+    });
   });
-};
-
-BitstampExchange.prototype.onMessage = function (message) {
-  if (!_.contains(this.channels, message.channel)) {
-    return;
-  }
-  var data = JSON.parse(message.data);
-
-  switch (message.event) {
-    case 'trade': this.emit('tick', 'trade', this.normalizeTrade(data)); break;
-    case 'data':  this.emit('tick', 'quote', this.normalizeQuote(data)); break;
-  }
+  socket.on('message', function (raw) {
+    var message = JSON.parse(raw);
+    if (!_.contains(self.channels, message.channel)) {
+      return;
+    }
+    var data = JSON.parse(message.data);
+    switch (message.event) {
+      case 'trade': self.emit('tick', 'trade', self.normalizeTrade(data)); break;
+      case 'data':  self.emit('tick', 'quote', self.normalizeQuote(data)); break;
+    }
+  });
 };
 
 BitstampExchange.prototype.normalizeQuote = function (tick) {
@@ -215,7 +132,7 @@ BitstampExchange.prototype.normalizeTrade = function (tick) {
   if (!tick) {
     return;
   }
-  trade = tick[0];
+  trade = util.isArray(tick) ? tick[0] : tick;
   return {
     ticker: this.ticker,
     id: trade['id'],
@@ -227,21 +144,105 @@ BitstampExchange.prototype.normalizeTrade = function (tick) {
 BitstampExchange.exchangeCode = 'bitstamp';
 
 
+// Btcchina
+// --------
+
+var BtcchinaExchange = exports.BtcchinaExchange = function (options) {
+  Exchange.call(this);
+
+  this.quotePollUrl = 'https://data.btcchina.com/data/orderbook?market=btccny&limit=10';
+  this.quotePollConcurrency = 1;
+  this.url = 'https://websocket.btcchina.com/';
+  this.channels = ['marketdata_cnybtc'];
+  this.ticker = config.tickers['btcchina'];
+};
+
+util.inherits(BtcchinaExchange, Exchange);
+
+BtcchinaExchange.prototype.start = function () {
+  var self = this;
+
+  // Streaming
+  var socket = new io(this.url);
+  socket.on('connect', function () {
+    socket.emit('subscribe', self.channels);
+    socket.on('trade', function (message) {
+      self.emit('tick', 'trade', self.normalizeTrade(message));
+    });
+  });
+
+  // Polling
+  (new Poller({
+    label: util.format('%s %s', this.exchangeCode, 'quote'),
+    url: this.quotePollUrl,
+    concurrency: this.quotePollConcurrency,
+    normalizer: this.normalizeQuote.bind(this),
+    comparator: function (new_, old) {
+      return new_.date > old.date;
+    }
+  })).on('tick', this.emit.bind(this, 'tick', 'quote')).start();
+
+};
+
+BtcchinaExchange.prototype.normalizeQuote = function (tick) {
+  if (!tick || !('bids' in tick) || !('asks' in tick)) {
+    return;
+  }
+  return {
+    ticker: this.ticker,
+    date: new Date(parseInt(tick['date']) * 1000),
+    asks: tick['asks'],
+    bids: tick['bids']
+  };
+};
+
+BtcchinaExchange.prototype.normalizeTrade = function (tick) {
+  if (!tick) {
+    return;
+  }
+  return {
+    ticker: this.ticker,
+    date: new Date(parseInt(tick['date']) * 1000),
+    id: tick['trade_id'],
+    price: tick['price'],
+    quantity: tick['amount'],
+    type: tick['type']
+  }
+};
+
+BtcchinaExchange.exchangeCode = 'btcchina';
+
+
 // Korbit
 // ------
 
 var KorbitExchange = exports.KorbitExchange = function (options) {
-  PollingExchange.call(this, _.defaults({
-    quotePollUrl: 'https://api.korbit.co.kr/v1/orderbook',
-    tradePollUrl: 'https://api.korbit.co.kr/v1/transactions',
-    quotePollConcurrency: 5,
-    tradePollConcurrency: 5
-  }, options));
+  Exchange.call(this);
 
+  this.quotePollUrl = 'https://api.korbit.co.kr/v1/orderbook';
+  this.tradePollUrl = 'https://api.korbit.co.kr/v1/transactions';
+  this.quotePollConcurrency = 2;
+  this.tradePollConcurrency = 2;
   this.ticker = config.tickers['korbit'];
 };
 
-util.inherits(KorbitExchange, PollingExchange);
+util.inherits(KorbitExchange, Exchange);
+
+KorbitExchange.prototype.start = function () {
+  (new Poller({
+    label: util.format('%s %s', this.exchangeCode, 'quote'),
+    url: this.quotePollUrl,
+    concurrency: this.quotePollConcurrency,
+    normalizer: this.normalizeQuote.bind(this)
+  })).on('tick', this.emit.bind(this, 'tick', 'quote')).start();
+
+  (new Poller({
+    label: util.format('%s %s', this.exchangeCode, 'trade'),
+    url: this.tradePollUrl,
+    concurrency: this.tradePollConcurrency,
+    normalizer: this.normalizeTrade.bind(this)
+  })).on('tick', this.emit.bind(this, 'tick', 'trade')).start();
+};
 
 KorbitExchange.prototype.normalizeQuote = function (tick) {
   if (!tick || !('bids' in tick) || !('asks' in tick)) {
